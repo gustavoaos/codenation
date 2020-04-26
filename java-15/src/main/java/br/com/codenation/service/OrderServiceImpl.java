6bd8fb067bf11a8cfcbceeac9a43c78c3feb7a93
Gustavo Alves <gustavo_alves@outlook.com>
2020-04-26 00:23:56 -0300

    codenation java-15: fetching desafio Order Service

---
+++ java-15/src/main/java/br/com/codenation/service/OrderServiceImpl.java
@@ -0,0 +1,48 @@
+package br.com.codenation.service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import br.com.codenation.model.OrderItem;
+import br.com.codenation.model.Product;
+import br.com.codenation.repository.ProductRepository;
+import br.com.codenation.repository.ProductRepositoryImpl;
+
+public class OrderServiceImpl implements OrderService {
+
+	private ProductRepository productRepository = new ProductRepositoryImpl();
+
+	/**
+	 * Calculate the sum of all OrderItems
+	 */
+	@Override
+	public Double calculateOrderValue(List<OrderItem> items) {
+		return null;
+	}
+
+	/**
+	 * Map from idProduct List to Product Set
+	 */
+	@Override
+	public Set<Product> findProductsById(List<Long> ids) {
+		return null;
+	}
+
+	/**
+	 * Calculate the sum of all Orders(List<OrderIten>)
+	 */
+	@Override
+	public Double calculateMultipleOrders(List<List<OrderItem>> orders) {
+		return null;
+	}
+
+	/**
+	 * Group products using isSale attribute as the map key
+	 */
+	@Override
+	public Map<Boolean, List<Product>> groupProductsBySale(List<Long> productIds) {
+		return null;
+	}
+
+}
