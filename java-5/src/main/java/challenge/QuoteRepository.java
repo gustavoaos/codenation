e30ee906d816ccf30439a362d99277f7be67f376
Gustavo Alves <gustavo_alves@outlook.com>
2020-04-28 01:25:58 -0300

    codenation java-5: utilizando count (w/ Example) para paginação de findByActor

--- java-5/src/main/java/challenge/QuoteRepository.java
+++ java-5/src/main/java/challenge/QuoteRepository.java
@@ -5,19 +5,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface QuoteRepository extends JpaRepository<Quote, Integer> {
 
-    long count();
-
-    // Page<Quote> findByActor(String actor, Pageable pageable);
+    Page<Quote> findByActor(String actor, Pageable pageable);
 
     Page<Quote> findAll(Pageable pageable);
-
-    // List<Quote> findAllByActor(String actor, Pageable pageable);
-
-    List<Quote> findByActor(String actor);
 
 }
--- java-5/src/main/java/challenge/QuoteServiceImpl.java
+++ java-5/src/main/java/challenge/QuoteServiceImpl.java
@@ -1,11 +1,10 @@
 package challenge;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class QuoteServiceImpl implements QuoteService {
@@ -16,7 +15,7 @@ public class QuoteServiceImpl implements QuoteService {
 	// https://stackoverflow.com/questions/24279186/fetch-random-records-using-spring-data-jpa/25149491
 	@Override
 	public Quote getQuote() {
-		int idx = (int) (Math.random() * this.repository.count());
+		int idx = this.generateRandomIdx(this.repository.count());
 
 		Page<Quote> page = this.repository.findAll(PageRequest.of(idx, 1));
 		Quote randomQuote = page.hasContent() ? page.getContent().get(0) : null;
@@ -26,12 +25,16 @@ public class QuoteServiceImpl implements QuoteService {
 
 	@Override
 	public Quote getQuoteByActor(String actor) {
-		List<Quote> quotes = this.repository.findByActor(actor); 
-		int idx = (int) (Math.random() * quotes.size());
+		int idx = this.generateRandomIdx(this.repository.count(Example.of(new Quote(actor))));
 
-		Quote randomQuote = quotes.isEmpty() ? null : quotes.get(idx);
+		Page<Quote> page = this.repository.findByActor(actor, PageRequest.of(idx, 1));
+		Quote randomQuote = page.hasContent() ? page.getContent().get(0) : null;
 
 		return randomQuote;
+	}
+
+	private int generateRandomIdx(long upperLimit) {
+		return (int) (Math.random() * upperLimit);
 	}
 
 }
