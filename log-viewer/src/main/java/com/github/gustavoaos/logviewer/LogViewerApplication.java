3d3b0e7bcaa39f6d364e683557f955b6742e8244
Gustavo Alves <gustavo_alves@outlook.com>
2020-05-03 13:42:41 -0300

    codenation projeto-pratico: iniciando projeto prático Central de Erros com Spring initializr

---
+++ log-viewer/src/main/java/com/github/gustavoaos/logviewer/LogViewerApplication.java
@@ -0,0 +1,13 @@
+package com.github.gustavoaos.logviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogViewerApplication.class, args);
+	}
+
+}
---
+++ log-viewer/src/test/java/com/github/gustavoaos/logviewer/LogViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gustavoaos.logviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
